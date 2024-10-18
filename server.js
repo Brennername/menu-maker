4480f216e7ae396606058780aca7653562fcbc34
danielremsburg <danielremsburg@gmail.com>
2024-10-18 04:43:15 +0000

    Heroku deployment

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/menu-maker'));
+app.use(express.static(__dirname + '/dist/menu-maker/browser'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/menu-maker/index.html'));
+res.sendFile(path.join(__dirname+'/dist/menu-maker/browser/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
